2200b905156ddbcb889f5d2b58255ad361ecb489
Bruno Arueira <bgnarueira@gmail.com>
2010-01-02 17:59:08 -0200

    Initial commit of README.txt and script_database.sql

---
+++ README.txt
@@ -0,0 +1,5 @@
+ExemploLombokJSF
+
+Feito para exemplificar a utilização do projeto Lombok (http://projectlombok.org) com o Java Server Faces (JSF).
+
+A intenção é demonstrar como o código contido nas classes Java fica menor e de certo modo mais legível com as anotações @Getter e @Setter nos métodos. 
---
+++ script_database.sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE "ExemploLombokJSF"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE noticias
+(
+  titulo character varying(40) NOT NULL,
+  codigo serial NOT NULL,
+  conteudo character varying(150) NOT NULL,
+  CONSTRAINT noticias_pk PRIMARY KEY (codigo)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE noticias OWNER TO postgres;
